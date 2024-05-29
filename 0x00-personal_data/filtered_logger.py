0918a99c17f9561f9778148367c8d93a6cb08012
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-29 00:25:54 -0700

    Task 4: ✨ Implemented main function to retrieve and log user data with redacted PII fields 🔒📋

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,8 @@ import logging
 from logging import Logger, StreamHandler
 import os
 import mysql.connector
+from filtered_logger import get_db, PII_FIELDS
+from datetime import datetime
 #from filtered_logger import RedactingFormatter
 
 
@@ -54,6 +56,28 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=db_name,
     )
     return connection
+
+
+def main():
+    """Logs the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 class RedactingFormatter(logging.Formatter):
