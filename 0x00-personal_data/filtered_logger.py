d307d5f71de16a2e09a46eff08693334e8fa95fe
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-30 22:45:41 -0700

    Task 3: reorganising the class code block

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -43,7 +43,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    
     connection = mysql.connector.connect(
         host=db_host,
         port=3306,
@@ -79,16 +78,17 @@ def main():
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ";"
 
+    
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
+    
     def format(self, record: logging.LogRecord) -> str:
         """formats a LogRecord.
         """
