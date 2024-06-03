62e01f4b3dee5b653940fee72994c439a8038dbe
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-03 12:50:22 -0700

    Task 3: 📁🔐 Create Auth class with basic authentication methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,12 +13,10 @@ class Auth:
         """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """Gets the authorization header field from the request.
         """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Gets the current user from the request.
