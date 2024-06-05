8bcb7b2fde3abf8ced138f1ec2cb6802cdda656f
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-05 04:38:32 -0700

    Task 1:🔄🔐 Implemented SessionAuth class and environment variable switch for authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""API module: Session authentication
+"""
+from .auth import Auth
+from models.user import User
+
+
+from uuid import uuid4
+
+
+user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
