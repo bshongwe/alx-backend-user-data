2b7c2a4b7f213ae3c0b95796818e221dfec70e38
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-10 02:52:04 -0700

    Task 7:✨ Implement endpoint to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,11 @@
 """
 from flask import Flask, jsonify
 
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -16,6 +19,22 @@ def index() -> str:
         - Home page's payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    Endpoint to register a user.
+
+    Return:
+        - The account creation payload.
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
